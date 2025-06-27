117dc81ff9312489d865d09c9346a158087797ba
datnopesuy09 <datntph43203@fpt.edu.vn>
2025-06-27 22:15:47 +0700

    Fixx lỗi

---
+++ da_be/src/main/java/com/example/da_be/da_beApplication.java
@@ -0,0 +1,13 @@
+package com.example.da_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class da_beApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(da_beApplication.class, args);
+    }
+
+}
